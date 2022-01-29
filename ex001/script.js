19ec385ce85ae6584a24034d0cb41ce3a9d46b78
Daniel Brícola <danielbricola@hotmail.com>
2022-01-29 18:37:51 -0300

    Ex001 completed

---
+++ ex001/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    // var minutos = data.getMinutes()
+    // var segundos = data.getSeconds()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if(hora >= 0 && hora < 12) {
+        //bom dia!
+        img.src = 'img/fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if(hora >= 12 && hora < 18) {
+        //boa tarde!
+        img.src = 'img/fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //boa noite!
+        img.src = 'img/fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
